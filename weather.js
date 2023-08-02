4f62a3c999208285ee4f5fff70c7e47770e2e5ee
Akshay-Comptivia <akshay.haridas@comptivia.com>
2023-08-02 09:07:35 +0530

    initial commit

---
+++ weather.js
@@ -0,0 +1,50 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    if ("geolocation" in navigator) {
+      // Geolocation is available
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+    
+
+          fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
+            .then((response) => response.json())
+            .then((data) => {
+              const locationName = data.address.city;
+              console.log("Current Location: ",locationName);
+              
+            })
+            .catch((error) => {
+              console.error("Error fetching location:", error);
+            });
+        },
+        function (error) {
+          console.error("Error getting geolocation:", error.message);
+        }
+      );
+    } else {
+      console.error("Geolocation is not available in this browser.");
+      
+    }
+    
+  });
+
+  function search(){
+    locationName=searchInput.value
+    console.log(locationName)
+
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=b41ec3be35c7dac8aabbc21ba253137a`).then((data)=>data.json()).then((data)=>{
+
+      const weatherDescription = data.weather[0].main;
+        tem.innerHTML= data.main.temp;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+        wet.innerHTML=weatherDescription
+        humi.innerHTML=humidity
+        speed.innerHTML=windSpeed
+      })
+      const weatherDetailsCard = document.getElementById("weather-details-card");
+  weatherDetailsCard.style.display = "block"; // Show the weather details card
+  }
+  
